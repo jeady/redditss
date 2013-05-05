b7e6d9aa824f98f18ba5178b655530b2f8b96186
James Eady <jmeady@gmail.com>
2013-05-05 11:24:40 -0700

    Add next link.

--- app/controllers/ss_controller.rb
+++ app/controllers/ss_controller.rb
@@ -1,11 +1,17 @@
 class SsController < ApplicationController
   def index
-    submissions = @api.read('/hot.json')
+    after = ''
+    if params.has_key? :after
+      after = "?after=#{params[:after]}"
+    end
+    submissions = @api.read("/hot.json#{after}")
     @img = submissions
-             .map { |s| {url: s.url, title: s.title} }
+             .map { |s| {url: s.url, title: s.title, id: s.id} }
              .select { |s| s[:url].include? 'imgur' }
              .reject { |s| s[:url].include? '/a/' }
              .reject { |s| s[:url].include? '/r/' }
              .each { |s| s[:url] += '.jpg' if !s[:url].ends_with?('.jpg') }
+
+    @next = @img[-1][:id]
   end
 end
